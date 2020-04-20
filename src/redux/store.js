c10bd54c6d5676ebd8e6a0a6aff8c16f11cdb203
Himanshu Jain <hkj4@dj4.edu>
2020-04-20 16:08:00 +0530

    Added local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
-
+import { persistStore } from "redux-persist";
 const middewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middewares));
+export const store = createStore(rootReducer, applyMiddleware(...middewares));
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
