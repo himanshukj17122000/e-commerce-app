945f91bdbdd2b515afd0e01c9c1a99495619a7d4
Himanshu Jain <hkj4@dj4.edu>
2020-05-20 17:59:20 +0530

    Added stripe

--- src/pages/checkout/checkout.page.jsx
+++ src/pages/checkout/checkout.page.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import "./checkout.styles.scss";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectCartItems } from "../../redux/cart/cart.selectors";
-import { selectPrice } from "../../redux/cart/cart.selectors";
-import CheckoutItem from "../../components/checkout-item/checkout-item.components";
+import React from 'react';
+import './checkout.styles.scss';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { selectPrice } from '../../redux/cart/cart.selectors';
+import CheckoutItem from '../../components/checkout-item/checkout-item.components';
+import StripCheckoutButton from '../../components/stripe-button/stripe-button.components';
 const Checkout = ({ cartItems, price }) => (
   <div className='checkout-page'>
     <div className='checkout-header'>
@@ -24,12 +25,18 @@ const Checkout = ({ cartItems, price }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => (
+    {cartItems.map(cartItem => (
       <CheckoutItem cartItem={cartItem} key={cartItem.id} />
     ))}
     <div className='total'>
       <span>TOTAL:${price}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+    </div>
+    <StripCheckoutButton price={price} />
   </div>
 );
 const mapsToProps = createStructuredSelector({
